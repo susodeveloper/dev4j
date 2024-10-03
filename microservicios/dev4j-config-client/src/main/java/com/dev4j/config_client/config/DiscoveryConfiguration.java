4f60f3abdc127c3e48ab242b150b8fb393447a12
suso <75487834+susodeveloper@users.noreply.github.com>
2024-10-03 10:07:23 +0200

    Add files via upload

---
+++ microservicios/dev4j-config-client/src/main/java/com/dev4j/config_client/config/DiscoveryConfiguration.java
@@ -0,0 +1,9 @@
+package com.dev4j.config_client.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DiscoveryConfiguration {
+}
