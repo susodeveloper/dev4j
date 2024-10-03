a4187a398b9bba2fada6ef3a843b3e2281ed2307
suso <75487834+susodeveloper@users.noreply.github.com>
2024-10-03 10:09:29 +0200

    Add files via upload

---
+++ microservicios/devs4j-game-of-thrones/src/main/java/com/devs4j/got/Devs4jGameOfThronesApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.got;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devs4jGameOfThronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devs4jGameOfThronesApplication.class, args);
+	}
+
+}
