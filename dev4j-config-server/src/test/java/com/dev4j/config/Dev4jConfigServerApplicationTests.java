aadc3bf55da0f8b2ea23d6b8020b6c0b06e4ee26
suso <75487834+susodeveloper@users.noreply.github.com>
2024-10-03 09:31:26 +0200

    Add files via upload

---
+++ dev4j-config-server/src/test/java/com/dev4j/config/Dev4jConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4j.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev4jConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
